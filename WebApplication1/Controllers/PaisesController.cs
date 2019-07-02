029e59253a8c5bf8ef56e8bb44e7dd98bad62dfa
Marcefv <marcefv.89@gmail.com>
2019-07-01 20:51:50 -0600

    Menu agregado

---
+++ WebApplication1/Controllers/PaisesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PaisesController : Controller
+    {
+        // GET: Paises
+        public ActionResult Informacion()
+        {
+            return View();
+        }
+
+
+        public ActionResult EntradaDatos()
+        {
+            return View();
+        }
+
+        public ActionResult SalidaDatos()
+        {
+            return View();
+        }
+
+
+        public ActionResult Beneficios()
+        {
+            return View();
+        }
+
+
+        public ActionResult Diferencias()
+        {
+            return View();
+        }
+
+    }
+}
